900b9374e3a9d09e5eb01e6864f6aa963faa3ffe
Yulia <yuliaolegyureva@gmail.com>
2024-02-16 18:51:58 +0700

    changed generated number so digits don't repeat

--- task1/src/main/java/RandNumber.java
+++ task1/src/main/java/RandNumber.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class RandNumber {
-    int number;
+    int number = 0;
+    ArrayList<Integer> digits = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
 
     public RandNumber(int min, int max) {
         Random rand = new Random();
-        number = rand.nextInt(max - min) + min;
+        int maxNumber = 9;
+        for (int i = 0; i < 4; i++) {
+            int minNumber = i == 0 ? 1 : 0;
+            number *= 10;
+            number += digits.remove(rand.nextInt(maxNumber - minNumber) + minNumber);
+            maxNumber -= 1;
+        }
     }
 
     public int getNumber() {
