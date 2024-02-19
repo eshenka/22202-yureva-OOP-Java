26ef57b26b8ea6bb8996f2f1fbe1e9557727e0fc
Yulia <yuliaolegyureva@gmail.com>
2024-02-19 14:59:15 +0700

    name convention

--- task1/src/main/java/Game/User.java
+++ task1/src/main/java/Game/User.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class User {
-    int input_number;
+    int inputNumber;
     public User() {
         System.out.println("Hello! Welcome to my new game: Bulls and Cows!\n");
     }
@@ -15,18 +15,18 @@ public class User {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            input_number = scanner.nextInt();
+            inputNumber = scanner.nextInt();
         } catch (InputMismatchException exception) {
             throw new Exception("Wrong symbols. Enter only 4-digit numbers!");
         }
 //        input_number = scanner.nextInt();
 
-        if (String.valueOf(input_number).length() != 4) {
+        if (String.valueOf(inputNumber).length() != 4) {
             throw new Exception("Bad length, try again!");
         }
     }
 
     public int getInputNumber() {
-        return input_number;
+        return inputNumber;
     }
 }
